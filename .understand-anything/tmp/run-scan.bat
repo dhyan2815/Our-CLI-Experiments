0bb7d88ba76e7273342724b23d10dbccde85645a
dhyan2815 <dhyan.work.2815@gmail.com>
2026-05-14 10:56:05 +0530

    chore: remove Understand Anything work tree

--- .understand-anything/tmp/run-scan.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-node "%~dp0ua-project-scan.js" "C:\Users\dhyan\Documents\DP Code's\AI\Agent Sandbox" "C:\Users\dhyan\Documents\DP Code's\AI\Agent Sandbox\.understand-anything\intermediate\scan-result.json"
